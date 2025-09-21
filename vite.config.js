31207556a857c953e7e616cba8676d0bf437ef17
PapiGBeats <dustinfusco67@gmail.com>
2025-09-20 22:09:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Papi-G-Beats/',   // 👈 this is the fix for GitHub Pages
   plugins: [react()],
 })
